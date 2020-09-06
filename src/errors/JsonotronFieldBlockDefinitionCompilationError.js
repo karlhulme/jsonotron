79b0087d9e8a5a688fed9cf004e6c0aa3aa4ae67
Karl Hulme <karlhulme@hotmail.com>
2020-09-06 16:10:52 +0100

    --feat Added the full stringify-d errors to the 2 JsonotronXyzError objects.

--- src/errors/JsonotronFieldBlockDefinitionCompilationError.js
+++ src/errors/JsonotronFieldBlockDefinitionCompilationError.js
@@ -3,7 +3,7 @@ import check from 'check-types'
 export class JsonotronFieldBlockDefinitionCompilationError extends Error {
   constructor (errors) {
     check.assert.array.of.object(errors)
-    super('The compilation of a Field Block Definition failed because the definition was not valid.')
+    super('The compilation of a Field Block Definition failed because the definition was not valid.\n\n' + JSON.stringify(errors, null, 2))
     this.name = this.constructor.name
     this.errors = errors
   }
--- src/errors/JsonotronInitialisationError.js
+++ src/errors/JsonotronInitialisationError.js
@@ -3,7 +3,7 @@ import check from 'check-types'
 export class JsonotronInitialisationError extends Error {
   constructor (errors) {
     check.assert.array.of.object(errors)
-    super('The Jsonotron initialisation process failed because the provided types were not valid.')
+    super('The Jsonotron initialisation process failed because the provided types were not valid.\n\n' + JSON.stringify(errors, null, 2))
     this.name = this.constructor.name
     this.errors = errors
   }
