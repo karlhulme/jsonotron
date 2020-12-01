09157a75549eb531f6b87f30603b4c9a91aa2140
Karl Hulme <karlhulme@hotmail.com>
2020-12-01 18:03:32 +0000

    --fix renamed the folders to honour the JSL naming.

--- test/types.test.js
+++ test/types.test.js
@@ -2,14 +2,12 @@ const { expect, test } = require('@jest/globals')
 const { Jsonotron } = require('jsonotron-js')
 const { readdirSync, readFileSync } = require('fs')
 
-test('The enum and schema types are valid.', () => {
-  const enumTypeFileNames = readdirSync('./types/enumTypes/')
-  const enumTypes = enumTypeFileNames.map(f => readFileSync('./types/enumTypes/' + f, 'utf8'))
+test('The enum and schema types of the jsonotron standard library are valid.', () => {
+  const enumTypeFileNames = readdirSync('./systems/jsl/enumTypes/')
+  const enumTypes = enumTypeFileNames.map(f => readFileSync('./systems/jsl/enumTypes/' + f, 'utf8'))
 
-  const schemaTypeFileNames = readdirSync('./types/schemaTypes/')
-  const schemaTypes = schemaTypeFileNames.map(f => readFileSync('./types/schemaTypes/' + f, 'utf8'))
+  const schemaTypeFileNames = readdirSync('./systems/jsl/schemaTypes/')
+  const schemaTypes = schemaTypeFileNames.map(f => readFileSync('./systems/jsl/schemaTypes/' + f, 'utf8'))
 
   expect(() => new Jsonotron({ types: enumTypes.concat(schemaTypes) })).not.toThrow()
 })
-
-
