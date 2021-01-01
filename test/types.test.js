4dfa1e394d23e53e793d9360a57aac25018c58f5
Karl Hulme <karlhulme@hotmail.com>
2021-01-01 13:12:26 +0000

    --fix Added field level documentation for the complex types.

--- test/types.test.js
+++ test/types.test.js
@@ -9,5 +9,9 @@ test('The enum and schema types of the jsonotron standard library are valid.', (
   const schemaTypeFileNames = readdirSync('./systems/jss/schemaTypes/')
   const schemaTypes = schemaTypeFileNames.map(f => readFileSync('./systems/jss/schemaTypes/' + f, 'utf8'))
 
-  expect(() => new Jsonotron({ types: enumTypes.concat(schemaTypes) })).not.toThrow()
+  const jsonotron = new Jsonotron({ types: enumTypes.concat(schemaTypes) })
+  expect(() => jsonotron.getEnumTypes()).not.toThrow()
+  expect(() => jsonotron.getSchemaTypes()).not.toThrow()
+  expect(() => jsonotron.getMarkdownForTypeSystem({ domain: 'https://jsonotron.org', system: 'jss', title: 'Docs', referencedTypeSystems: [] })).not.toThrow()
+  expect(() => jsonotron.getGraphQLDefsForTypeSystems({})).not.toThrow()
 })
