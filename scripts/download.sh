99bb7aec2eb845f3ec1481e332a04dfbf1c286a3
Karl Hulme <karlhulme@hotmail.com>
2021-01-29 18:14:57 +0000

    --feat Moved the download script to the mono-repo parent and rename to reflect that it downloads all systems.

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,12 +2,12 @@
 mkdir -p download
 rm -rf ./download/*
 
-# Download a specific version of the Jsonotron types and schemas
+# Download a specific version of the Jsonotron repo
 curl -L \
-  https://api.github.com/repos/karlhulme/jsonotron/tarball/v13.1.2 \
+  https://api.github.com/repos/karlhulme/jsonotron/tarball/v15.0.0 \
   --compressed \
   -o download/jsonotron.tar.gz
 
-# Extract the types and schemas
+# Extract all the enum and schema types
 gtar -x -f download/jsonotron.tar.gz --strip-components=1 -C download --wildcards "**/enumTypes/*.yaml"
 gtar -x -f download/jsonotron.tar.gz --strip-components=1 -C download --wildcards "**/schemaTypes/*.yaml"
