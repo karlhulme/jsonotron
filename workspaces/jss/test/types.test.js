601650fac84c6473bd9353f4e098c32052bbb840
Karl Hulme <karlhulme@hotmail.com>
2021-03-06 17:06:58 +0000

    --fix Changed the jss validation to produce a typemap rather than relying on markdown generation.

--- workspaces/jss/test/types.test.js
+++ workspaces/jss/test/types.test.js
@@ -10,7 +10,5 @@ test('The enum and schema types of the jsonotron standard library are valid.', (
   const schemaTypes = schemaTypeFileNames.map(f => readFileSync('./schemaTypes/' + f, 'utf8'))
 
   const jsonotron = new Jsonotron({ types: enumTypes.concat(schemaTypes) })
-  expect(() => jsonotron.getEnumTypes()).not.toThrow()
-  expect(() => jsonotron.getSchemaTypes()).not.toThrow()
-  expect(() => jsonotron.getMarkdownForTypeSystem({ domain: 'https://jsonotron.org', system: 'jss', title: 'Docs', referencedTypeSystems: [] })).not.toThrow()
+  expect(() => jsonotron.getTypeMap(['https://jsonotron.org/jss'])).not.toThrow()
 })
