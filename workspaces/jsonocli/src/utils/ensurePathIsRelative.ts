131ddc489dc49f1ef3c7064c335d65e8777223ae
Karl Hulme <karlhulme@hotmail.com>
2021-05-01 20:03:38 +0100

    --feat Drop jsonocli tool, as can now use curl instead.

--- workspaces/jsonocli/src/utils/ensurePathIsRelative.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Raises an error if the given path is absolute.
- * @param path A path.
- */
-export function ensurePathIsRelative (path: string): void {
-  if (!path.startsWith('.')) {
-    throw new Error('Path should be relative.')
-  }
-}
--- workspaces/jsonocli/src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ensurePathDirectoriesExist'
-export * from './ensurePathIsRelative'
