753177eb08ac57026489ffbe3cb6bc3dbdfaf475
Karl Hulme <karlhulme@hotmail.com>
2021-05-02 07:24:48 +0100

    --fix Ensure creation of output directory when creating example codegen.

--- workspaces/examples/test/langTemplates.test.ts
+++ workspaces/examples/test/langTemplates.test.ts
@@ -1,18 +1,9 @@
 import { test } from '@jest/globals'
 import fg from 'fast-glob'
-import { readFile } from 'fs/promises'
+import { mkdir, readFile } from 'fs/promises'
 import { writeFile } from 'fs/promises'
 import { parseTypeLibrary } from 'jsonotron-js'
 import { loadTemplatesFromFolder, createTemplateProcessor, TemplateProcessorContext } from 'jsonotron-codegen'
-
-function chooseOutputName (templateName: string): string {
-  switch (templateName) {
-    case 'graphql': return 'graphql.gql'
-    case 'markdown': return 'markdown.md'
-    case 'typescript': return 'typescript.ts'
-    default: return `${templateName}.txt`
-  }
-}
 
 test('Generate output for each language.', async () => {
   const typeFileNames = await fg('./src/typeLibrary/**/*.yaml')
@@ -30,7 +21,23 @@ test('Generate output for each language.', async () => {
     const processor = createTemplateProcessor(template)
 
     const result = processor(context)
+    const outputFilePath = `./output/${chooseOutputName(template.name)}`
 
-    await writeFile(`./output/${chooseOutputName(template.name)}`, result, 'utf-8')
+    await ensureDirectory(outputFilePath)
+    await writeFile(outputFilePath, result, 'utf-8')
   }))
 })
+
+async function ensureDirectory (path: string): Promise<void> {
+  const lastDividerIndex = path.lastIndexOf('/')
+  await mkdir(path.slice(0, lastDividerIndex), { recursive: true })
+}
+
+function chooseOutputName (templateName: string): string {
+  switch (templateName) {
+    case 'graphql': return 'graphql.gql'
+    case 'markdown': return 'markdown.md'
+    case 'typescript': return 'typescript.ts'
+    default: return `${templateName}.txt`
+  }
+}
